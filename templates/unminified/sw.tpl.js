c2544faf1e595e0319ae96ee8e06433620a914f7
Guillaume C. Marty <gmarty@users.noreply.github.com>
2016-06-13 12:19:59 +0100

    Inline files to load. r=arcturus (#8)

--- templates/unminified/sw.tpl.js
+++ templates/unminified/sw.tpl.js
@@ -1,9 +1,8 @@
-importScripts('files.js');
 importScripts('sww.js');
 
 var version = '$VERSION';
 var worker = new self.ServiceWorkerWare();
-worker.use(new self.StaticCacher(FILES_TO_LOAD));
+worker.use(new self.StaticCacher($FILES_TO_LOAD));
 worker.use(new self.SimpleOfflineCache());
 
 var extraFile = '$HOOK';
